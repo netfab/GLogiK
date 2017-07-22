bf39161b62606f080260f13d1e4b31415ef65f70
netfab <netbox253@gmail.com>
2017-07-22 15:28:53 +0200

    Remove old code.

--- src/bin/daemon/main.cpp
+++ src/bin/daemon/main.cpp
@@ -19,24 +19,13 @@
  *
  */
 
-//#include <iostream>
-//#include <cstdlib>
-
 #include "daemon.h"
 #include "exception.h"
 
 using namespace GLogiKd;
 
 int main(int argc, char *argv[]) {
-	//try {
-		GLogiKDaemon daemon;
-		return daemon.run(argc, argv);
-	/*
-	}
-	catch ( const GLogiKExcept & e ) {
-		std::cerr << e.what() << "\n";
-	}
-	return EXIT_FAILURE;
-	*/
+	GLogiKDaemon daemon;
+	return daemon.run(argc, argv);
 }
 
