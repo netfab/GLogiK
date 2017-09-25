c5a601166b66ab9ea846f3dc81f09bbc3319f8e6
netfab <netbox253@gmail.com>
2017-09-25 11:12:12 +0200

    Rename pointers.

--- src/bin/daemon/devices_manager.cpp
+++ src/bin/daemon/devices_manager.cpp
@@ -495,23 +495,25 @@ void DevicesManager::startMonitoring(GKDBus* DBus) {
 	this->fds[0].events = POLLIN;
 
 	//DBus->addSignalMatch(BusConnection::GKDBUS_SESSION, "test.signal.Type");
-	const char* object_path = "DeviceManager";
-	const char* device_interface = "com.glogik.Daemon.Device1";
+	{
+		const char* devices_object = "DeviceManager";
+		const char* devices_interface = "com.glogik.Daemon.Device1";
 
-	DBus->addEvent_StringToBool_Callback( object_path, device_interface, "Stop",
-		{	{"s", "device_id", "in", "device ID coming from ..."}, // FIXME
-			{"b", "is_stop_succeeded", "out", "does the Stop method succeeded ?"} },
-		std::bind(&DevicesManager::stopDevice, this, std::placeholders::_1) );
+		DBus->addEvent_StringToBool_Callback( devices_object, devices_interface, "Stop",
+			{	{"s", "device_id", "in", "device ID coming from ..."}, // FIXME
+				{"b", "is_stop_succeeded", "out", "does the Stop method succeeded ?"} },
+			std::bind(&DevicesManager::stopDevice, this, std::placeholders::_1) );
 
-	DBus->addEvent_StringToBool_Callback( object_path, device_interface, "Start",
-		{	{"s", "device_id", "in", "device ID coming from ..."}, // FIXME
-			{"b", "is_start_succeeded", "out", "does the Start method succeeded ?"} },
-		std::bind(&DevicesManager::startDevice, this, std::placeholders::_1) );
+		DBus->addEvent_StringToBool_Callback( devices_object, devices_interface, "Start",
+			{	{"s", "device_id", "in", "device ID coming from ..."}, // FIXME
+				{"b", "is_start_succeeded", "out", "does the Start method succeeded ?"} },
+			std::bind(&DevicesManager::startDevice, this, std::placeholders::_1) );
 
-	DBus->addEvent_StringToBool_Callback( object_path, device_interface, "Restart",
-		{	{"s", "device_id", "in", "device ID coming from ..."}, // FIXME
-			{"b", "is_restart_succeeded", "out", "does the Restart method succeeded ?"} },
-		std::bind(&DevicesManager::restartDevice, this, std::placeholders::_1) );
+		DBus->addEvent_StringToBool_Callback( devices_object, devices_interface, "Restart",
+			{	{"s", "device_id", "in", "device ID coming from ..."}, // FIXME
+				{"b", "is_restart_succeeded", "out", "does the Restart method succeeded ?"} },
+			std::bind(&DevicesManager::restartDevice, this, std::placeholders::_1) );
+	}
 
 	LOG(DEBUG2) << "loading known drivers";
 
