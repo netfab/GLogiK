8c67c064e225d5f554aeab5242a94bbcbf94b075
netfab <netbox253@gmail.com>
2018-01-03 12:56:13 +0100

    Happy New Year ©.

--- src/lib/utils/log.h
+++ src/lib/utils/log.h
@@ -19,7 +19,7 @@
  *
  *	This file is part of GLogiK project.
  *	GLogiK, daemon to handle special features on gaming keyboards
- *	Copyright (C) 2016-2017  Fabrice Delliaux <netbox253@gmail.com>
+ *	Copyright (C) 2016-2018  Fabrice Delliaux <netbox253@gmail.com>
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
