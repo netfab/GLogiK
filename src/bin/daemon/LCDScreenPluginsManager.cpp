73e34e550031ac3264d6fb3bf933288200aa89f4
netfab <netbox253@gmail.com>
2018-05-10 09:05:28 +0200

    Add block of comments.

--- src/bin/daemon/LCDScreenPluginsManager.cpp
+++ src/bin/daemon/LCDScreenPluginsManager.cpp
@@ -66,7 +66,60 @@ void LCDScreenPluginsManager::stopLCDPlugins(void) {
 	this->plugins_.clear();
 }
 
-
+/*
+ * -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+ * PBM data binary format (without header), for a set of bytes (A, B, C, ...)
+ * -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+ *	A7 A6 A5 A4 A3 A2 A1 A0 B7 B6 B5 B4 B3 B2 B1 B0  .  .
+ *	U7 U6 U5 U4 U3 U2 U1 U0  .  .  .  .  .  .  .  .  .  . --->
+ *	 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . ---> on PBM_WIDTH_IN_BYTES bytes
+ *	 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . --->
+ *	 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+ *	                      |  |  |
+ *	                       \   /
+ *	                        \ /
+ *	                 on PBM_HEIGHT bytes
+ *
+ * -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+ * LCD data binary format (without header). Description coming from libg15.
+ * For a set of bytes (A, B, C, etc.) the bits representing pixels will
+ * appear on the LCD like this.
+ * -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+ *	A0 B0 C0
+ *	A1 B1 C1
+ *	A2 B2 C2
+ *	A3 B3 C3 ... and across for G15_LCD_WIDTH bytes
+ *	A4 B4 C4
+ *	A5 B5 C5
+ *	A6 B6 C6
+ *	A7 B7 C7
+ *
+ *	A0
+ *	A1	<- second 8-pixel-high row starts straight after the last byte on
+ *	A2		the previous row
+ *	A3
+ *	A4
+ *	A5
+ *	A6
+ *	A7
+ *
+ *	A0
+ *	 .
+ *	 .
+ *
+ *	A0
+ *	 .
+ *	 .
+ *
+ *	A0
+ *	 .
+ *	 .
+ *
+ *	A0
+ *	A1	<- only the first three bits are shown on the bottom row (the last three
+ *	A2		pixels of the 43-pixel high display.)
+ *
+ */
 void LCDScreenPluginsManager::dumpPBMDataIntoLCDBuffer(PBMDataArray & lcd_buffer, const PBMDataArray & pbm_data)
 {
 	for(unsigned int row = 0; row < PBM_HEIGHT_IN_BYTES; ++row) {
