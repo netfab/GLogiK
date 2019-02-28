ab98282851e0d911e9131e8f1a2f753961aac003
netfab <netbox253@gmail.com>
2019-02-28 17:33:05 +0100

    Always daemonize, even in debug mode.
    Else the init script will obviously hang on boot if build with debug on.

--- src/bin/daemon/daemon.cpp
+++ src/bin/daemon/daemon.cpp
@@ -2,7 +2,7 @@
  *
  *	This file is part of GLogiK project.
  *	GLogiK, daemon to handle special features on gaming keyboards
- *	Copyright (C) 2016-2018  Fabrice Delliaux <netbox253@gmail.com>
+ *	Copyright (C) 2016-2019  Fabrice Delliaux <netbox253@gmail.com>
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -124,13 +124,10 @@ int GLogiKDaemon::run( const int& argc, char *argv[] ) {
 		this->parseCommandLine(argc, argv);
 
 		if( GLogiKDaemon::isDaemonRunning() ) {
-#if DEBUGGING_ON == 0
+
 			_pid = daemonizeProcess(true);
 			syslog(LOG_INFO, "process successfully daemonized");
-#else
-			_pid = getpid();
-			LOG(DEBUG) << "process not daemonized - debug ON - pid: " << _pid;
-#endif
+
 			this->createPIDFile();
 
 			// TODO handle return values and errno ?
