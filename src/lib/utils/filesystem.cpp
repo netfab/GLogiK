4c98ba71530f754313a92db5b9088ee67294fb9f
netfab <netbox253@gmail.com>
2021-06-02 15:26:35 +0200

    Remove old logging system.

--- src/lib/utils/filesystem.cpp
+++ src/lib/utils/filesystem.cpp
@@ -22,13 +22,9 @@
 #include <syslog.h>
 
 #include <cstdint>
-//#include <cerrno>
-#include <cstring>
-#include <cstdlib>
 
 #include <algorithm>
 #include <sstream>
-//#include <chrono>
 
 #include <config.h>
 
@@ -40,8 +36,6 @@
 #include "filesystem.hpp"
 
 #undef UTILS_COMPILATION
-
-//namespace chr = std::chrono;
 
 namespace NSGKUtils
 {
@@ -156,89 +150,6 @@ void FileSystem::traceLastDirectoryCreation(void)
 	}
 }
 #endif
-
-/*
-#if DEBUGGING_ON
-void FileSystem::openDebugFile(
-	const std::string & baseName,
-	FILE* & pFile,
-	const fs::perms prms,
-	const bool createDir
-	)
-{
-	if( LOG_TO_FILE_AND_CONSOLE::FileReportingLevel() == NONE ) {
-		if( pFile == nullptr) {
-			syslog(LOG_INFO, "debug file not opened");
-			return;
-		}
-	}
-
-	fs::path debugFile(DEBUG_DIR);
-
-	if( createDir )
-		FileSystem::createDirectory(debugFile, fs::owner_all | fs::group_all);
-
-	{
-		chr::seconds s = chr::duration_cast< chr::seconds >(
-			chr::system_clock::now().time_since_epoch()
-		);
-
-		debugFile /= baseName;
-		debugFile += "-";
-		debugFile += std::to_string(s.count());
-	}
-
-	debugFile += "-XXXXXX.log";
-
-	int fd = -1;
-	std::string filePath;
-
-	{
-		const std::string f( debugFile.string() );
-		char char_array[f.size() + 1];
-
-		std::strcpy(char_array, f.c_str());
-
-		fd = mkstemps(char_array, 4);
-		filePath = char_array;
-	}
-
-	auto openError = [&filePath] (const std::string & error) -> void {
-		std::ostringstream buffer(std::ios_base::app);
-		buffer << error << " : " << filePath;
-		if(errno != 0) {
-			buffer << " : " << strerror(errno);
-		}
-
-		syslog(LOG_ERR, "%s", buffer.str().c_str());
-	};
-
-	if( fd != -1 ) {
-		pFile = fdopen(fd, "w");
-		if(pFile != nullptr) {
-			if( prms != fs::no_perms ) {
-				boost::system::error_code ec;
-				fs::permissions(filePath, prms, ec);
-				if( ec.value() != 0 ) {
-					std::ostringstream buffer(std::ios_base::app);
-					buffer << "failed to set file permissions : " << filePath
-							<< " : " << ec.message();
-					GKSysLogError(buffer.str());
-				}
-			}
-
-			LOG_TO_FILE_AND_CONSOLE::FileStream() = pFile;
-		}
-		else openError("fdopen error");
-	}
-	else openError("mkstemps error");
-
-	// tried to open debug file but something goes wrong
-	if( pFile == nullptr)
-		syslog(LOG_INFO, "debug file not opened");
-}
-#endif
-*/
 
 } // namespace NSGKUtils
 
