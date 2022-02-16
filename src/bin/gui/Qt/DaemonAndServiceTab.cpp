87e1b294da8721055d82d1aae2a51689ecd796d0
Fabrice Delliaux <netbox253@gmail.com>
2022-02-16 19:14:58 +0100

    Qt gui: declare pointer in try block

--- src/bin/gui/Qt/DaemonAndServiceTab.cpp
+++ src/bin/gui/Qt/DaemonAndServiceTab.cpp
@@ -2,7 +2,7 @@
  *
  *	This file is part of GLogiK project.
  *	GLogiK, daemon to handle special features on gaming keyboards
- *	Copyright (C) 2016-2021  Fabrice Delliaux <netbox253@gmail.com>
+ *	Copyright (C) 2016-2022  Fabrice Delliaux <netbox253@gmail.com>
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -59,10 +59,8 @@ void DaemonAndServiceTab::buildTab(void)
 {
 	GK_LOG_FUNC
 
-	QVBoxLayout* vBox = nullptr;
-
 	try {
-		vBox = new QVBoxLayout(this);
+		QVBoxLayout* vBox = new QVBoxLayout(this);
 		GKLog(trace, "allocated QVBoxLayout")
 
 		this->setLayout(vBox);
