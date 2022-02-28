a332e81567d943c5fb1b21c6866ec6b5c687bd97
Fabrice Delliaux <netbox253@gmail.com>
2022-02-28 16:40:41 +0100

    DevicesManager: implement getDeviceMKeysNames()

--- src/bin/daemon/devicesManager.hpp
+++ src/bin/daemon/devicesManager.hpp
@@ -97,6 +97,8 @@ class DevicesManager
 		const banksMap_type & getDeviceMacrosBanks(const std::string & devID) const;
 		const std::vector<std::string> &
 			getDeviceGKeysNames(const std::string & devID) const;
+		const std::vector<std::string> &
+			getDeviceMKeysNames(const std::string & devID) const;
 		const std::string getDeviceStatus(const std::string & devID) const;
 
 	protected:
