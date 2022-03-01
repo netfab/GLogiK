793ec08ee054400f7789f4d980861c1534bd4e8f
Fabrice Delliaux <netbox253@gmail.com>
2022-03-01 16:46:46 +0100

    ClientsManager: expose bank names D-Bus method

--- src/bin/daemon/clientsManager.cpp
+++ src/bin/daemon/clientsManager.cpp
@@ -201,6 +201,13 @@ void ClientsManager::initializeDBusRequests(NSGKDBus::GKDBus* pDBus)
 			{"as", "array_of_strings", dOUT, "string array of device macro keys names"} },
 		std::bind(&ClientsManager::getDeviceGKeysNames, this, std::placeholders::_1, std::placeholders::_2) );
 
+	_pDBus->NSGKDBus::EventGKDBusCallback<TwoStringsToStringsArray>::exposeMethod(
+		system_bus, DM_object, DM_interf, "GetDeviceMKeysNames",
+		{	{"s", "client_unique_id", dIN, "must be a valid client ID"},
+			{"s", "device_id", dIN, "device ID coming from GetStartedDevices or GetStoppedDevices"},
+			{"as", "array_of_strings", dOUT, "string array of M-keys names"} },
+		std::bind(&ClientsManager::getDeviceMKeysNames, this, std::placeholders::_1, std::placeholders::_2) );
+
 	_pDBus->NSGKDBus::EventGKDBusCallback<TwoStringsThreeBytesToBool>::exposeMethod(
 		system_bus, DM_object, DM_interf, "SetDeviceBacklightColor",
 		{	{"s", "client_unique_id", dIN, "must be a valid client ID"},
