c8b8e8a8e48ff185ff3d1332f4cb565816380bf2
Fabrice Delliaux <netbox253@gmail.com>
2025-09-01 07:25:11 +0200

    utils: fix random generator initialization misbehavior
    Since 3fae87511 random generator was unable to properly initialize
    because of default contructor. Coming back to previous state, and
    declare default charset static.
    
    Signed-off-by: Fabrice Delliaux <netbox253@gmail.com>

--- src/lib/utils/randomGenerator.hpp
+++ src/lib/utils/randomGenerator.hpp
@@ -36,8 +36,7 @@ namespace NSGKUtils
 class RandomGenerator
 {
 	public:
-		RandomGenerator(void) : RandomGenerator(_defaultCharset) {};
-		RandomGenerator(const std::vector<char> & charset);
+		RandomGenerator(const std::vector<char> & charset = RandomGenerator::defaultCharset);
 		~RandomGenerator(void);
 
 		const std::string getString(std::size_t length);
@@ -49,12 +48,7 @@ class RandomGenerator
 		std::default_random_engine _rng;
 		std::uniform_int_distribution<> _dist;
 
-		const std::vector<char> _defaultCharset = {
-			'0','1','2','3','4','5','6','7','8','9',
-			'a','b','c','d','e','f','g','h','i','j',
-			'k','l','m','n','o','p','q','r','s','t',
-			'u','v','w','x','y','z'
-		};
+		static const std::vector<char> defaultCharset;
 };
 
 } // namespace NSGKUtils
